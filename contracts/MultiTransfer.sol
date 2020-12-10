4b4599851d141afb3100b4e3bb3f2b4fa2218c27
Jeff Garzik <jgarzik@localhost.localdomain>
2020-12-10 09:39:47 +0000

    MultiTransfer: remove 'amountIn' check parameter This was not required, and will revert anyway if under-funded. Therefore, removed, to save gas.

--- contracts/MultiTransfer.sol
+++ contracts/MultiTransfer.sol
@@ -14,25 +14,18 @@ contract MultiTransfer is IMultiTransfer {
     /// @return true/false
     function multiTransfer(
         address erc20,
-        uint256 amountIn,
         uint256[] calldata bits
     ) external override returns (bool) {
         require(erc20 != address(0), "ERC20 address invalid");
-        require(amountIn != 0, "Input amount invalid");
 
         IERC20 token = IERC20(erc20);
-        uint256 totalOut = 0;
 
         // output amount to recipients
         for (uint256 i = 0; i < bits.length; i++) {
             address a = address(bits[i] >> 96);
             uint256 amount = bits[i] & ((1 << 96) - 1);
             token.safeTransferFrom(msg.sender, a, amount);
-
-            totalOut += amount;
         }
-
-        require(amountIn == totalOut, "Output amt must equal input amt");
 
         return true;
     }
--- contracts/interfaces/IMultiTransfer.sol
+++ contracts/interfaces/IMultiTransfer.sol
@@ -5,7 +5,6 @@ pragma solidity 0.6.12;
 interface IMultiTransfer {
     function multiTransfer(
         address erc20,
-        uint256 amountIn,
         uint256[] calldata bits
     ) external returns (bool);
 }
