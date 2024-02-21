6386283a763aa7e3d4ca119ac98dfc7964c55a6e
Rohit Solia <rohit.solia@gmail.com>
2024-02-21 14:44:51 -0600

    Updated contract source code verify step

--- deploy/02_MultiTransfer.ts
+++ deploy/02_MultiTransfer.ts
@@ -1,4 +1,4 @@
-import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { HardhatRuntimeEnvironment, HttpNetworkConfig } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
 
 const name = 'MultiTransfer'
@@ -11,9 +11,15 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   const deployed = await deploy(name, { from: deployer, log: true })
 
-  if (hre.network.name !== 'localhost') {
+  const networkConfig = hre.network.config as unknown as HttpNetworkConfig
+  if (hre.network.name !== 'localhost' && !networkConfig.url.includes('localhost')) {
     console.log('Verifying source code on the block explorer')
-    await run('verify', { address: deployed.address, noCompile: true })
+    try {
+      await run('verify:verify', { address: deployed.address, noCompile: true })
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (e: any) {
+      console.error('Contract verification failed for %s at %s', name, deployed.address, e.message)
+    }
   }
 
   func.id = `${name}-${version}`
